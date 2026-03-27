2c2b6df28578269f985827655e193e35b3f2d291
Kévin ROBERT <kevin.robert.kr@gmail.com>
2026-03-27 20:54:54 +0100

    Init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # krosoft-tooling-prettier
+
+[![forthebadge](https://forthebadge.com/images/badges/built-with-love.svg)](https://forthebadge.com)
---
+++ index.js
@@ -0,0 +1,18 @@
+/** @type {import("prettier").Config} */
+const config = {
+  semi: true,
+  trailingComma: "all",
+  singleQuote: false,
+  printWidth: 160,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  arrowParens: "avoid",
+  endOfLine: "auto",
+  bracketSameLine: false,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  htmlWhitespaceSensitivity: "ignore",
+};
+
+export default config;
